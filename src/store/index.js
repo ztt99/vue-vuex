4941ac5051aaf22498f9202d64f290159c07a969
zhangting <343185404@qq.com>
2021-01-24 16:11:28 +0800

    初始化

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)   //挂载，调用install方法
+
+const store = new Vuex.Store({  //创建vuex实例
+    state:{
+        name : 'zs'
+    },
+    mutations:{
+
+    },
+    getters :{
+
+    },
+    actions:{
+
+    },
+
+})
+
+export default store
+
